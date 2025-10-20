fad0ebd4bc7a381c9dc4d372a0eceaccb2c89e47
ShroukOuda <s36568961@gmail.com>
2025-10-20 12:22:56 +0300

    register AppDbContext

--- E-Commerece.Infrastructure/InfrastructureRegisteration.cs
+++ E-Commerece.Infrastructure/InfrastructureRegisteration.cs
@@ -1,16 +1,26 @@
 using E_Commerece.Core.Interfaces;
+using E_Commerece.Infrastructure.Data;
 using E_Commerece.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace E_Commerece.Infrastructure;
 
 public static class InfrastructureRegisteration
 {
-    public static IServiceCollection InfrastructureConfiguratoin(this IServiceCollection services)
+    public static IServiceCollection InfrastructureConfiguratoin(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            
+        
+        //unit of work
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        //db context
+        services.AddDbContext<AppDbContext>(option =>
+        {
+            option.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
         return services;
     }
 }

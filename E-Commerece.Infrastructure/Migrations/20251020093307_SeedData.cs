bbf4e8c06f97085a546ef0102c92cd55b72efe32
ShroukOuda <s36568961@gmail.com>
2025-10-20 12:34:39 +0300

    Add Seed Data

---
+++ E-Commerece.Infrastructure/Migrations/20251020093307_SeedData.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace E_Commerece.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Devices, gadgets, and electronic accessories", "Electronics" },
+                    { 2, "Men’s, women’s, and children’s fashion items", "Clothing" },
+                    { 3, "Appliances, furniture, and home improvement tools", "Home & Kitchen" },
+                    { 4, "Cosmetics, skincare, and healthcare products", "Beauty & Health" },
+                    { 5, "Educational, fiction, and non-fiction books", "Books" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Apple smartphone with A17 chip and dual camera system.", "iPhone 15", 1199.99m },
+                    { 2, 2, "Stylish blue denim jacket for men.", "Men’s Denim Jacket", 89.5m },
+                    { 3, 3, "Compact kitchen microwave with digital controls.", "Microwave Oven", 230m },
+                    { 4, 4, "Gentle foaming face wash suitable for all skin types.", "Facial Cleanser", 15.75m },
+                    { 5, 5, "Comprehensive book on C# and .NET development.", "C# Programming Guide", 39.99m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}

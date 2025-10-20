fcfc185d93b15b9d8c5e7152cd39a2e87220ab6e
ShroukOuda <s36568961@gmail.com>
2025-10-20 11:32:38 +0300

    add GenericRepository

---
+++ E-Commerece.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,66 @@
+using System.Linq.Expressions;
+using E_Commerece.Core.Interfaces;
+using E_Commerece.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerece.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+    {
+        var query = _context.Set<T>().AsQueryable();
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+        return await query.ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        return entity;
+    }
+
+    public async Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _context.Set<T>();
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+        var entity = await query.FirstOrDefaultAsync(x => EF.Property<int>(x, "Id") == id);
+        return entity;
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}

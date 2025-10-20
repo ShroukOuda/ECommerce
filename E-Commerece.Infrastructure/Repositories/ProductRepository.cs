97aad3c2c60f5d449b9648782dd5eb58edf63b90
ShroukOuda <s36568961@gmail.com>
2025-10-20 11:32:09 +0300

    add ProductRepository

---
+++ E-Commerece.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+using E_Commerece.Core.Entites.Product;
+using E_Commerece.Core.Interfaces;
+using E_Commerece.Infrastructure.Data;
+
+namespace E_Commerece.Infrastructure.Repositories;
+
+public class ProductRepository : GenericRepository<Product>, IProductRepository
+{
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+    }
+}

5f65b1ec6ad5bb9be10262f43ed679c945ead7b0
ShroukOuda <s36568961@gmail.com>
2025-12-05 11:52:18 +0200

    add created and update properties

--- E-Commerece.Core/Entites/BaseEntity.cs
+++ E-Commerece.Core/Entites/BaseEntity.cs
@@ -3,4 +3,6 @@ namespace E_Commerece.Core.Entites;
 public class BaseEntity<T>
 {
     public T Id { get; set; }
+    public  DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }

0b4f3e014bfe1ef86865151430377b88f80b74ae
ShroukOuda <s36568961@gmail.com>
2025-12-05 11:49:59 +0200

    add ProductParams, PaginationParams

---
+++ E-Commerece.Core/Models/ProductParams.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace E_Commerece.Core.Models;
+
+public class ProductParams : PaginationParams
+{
+    public string? Search { get; set; }
+    public int? CategoryId { get; set; }
+    public float? MaxPrice { get; set; } = null;
+    public float? MinPrice { get; set; } = null;
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public ProductSortBy SortBy { get; set; } = ProductSortBy.NameAsc;
+    
+    public void ValidatePrices()
+    {
+        if (MinPrice.HasValue && MaxPrice.HasValue && MinPrice > MaxPrice)
+            throw new ArgumentException("MinPrice cannot be greater than MaxPrice.");
+    }
+
+}
---
+++ E-Commerece.Core/Models/ProductSortBy.cs
@@ -0,0 +1,11 @@
+namespace E_Commerece.Core.Models;
+
+public enum ProductSortBy
+{
+    NameAsc,
+    NameDesc,
+    PriceAsc,
+    PriceDesc,
+    Newest,
+    Oldest
+}

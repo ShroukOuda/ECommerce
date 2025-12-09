3d1f829780b0582983cd003a8c78db7501fa3439
ShroukOuda <s36568961@gmail.com>
2025-12-09 05:50:21 +0200

    set default PageNumber to 1 in PaginationParams

--- E-Commerece.Core/Models/PaginationParams.cs
+++ E-Commerece.Core/Models/PaginationParams.cs
@@ -3,7 +3,7 @@ namespace E_Commerece.Core.Models;
 public class PaginationParams
 {
     private const int MaxPageSize = 20;
-    public int  PageNumber { get; set; }
+    public int PageNumber { get; set; } = 1;
     private int _pageSize = 10;
 
     public int PageSize

00783051d48eee644f9b662d63d332ac0621e8dc
ShroukOuda <s36568961@gmail.com>
2025-12-09 07:43:40 +0200

    add file settings configuration and reorder image management service registration

--- E-Commerece.Infrastructure/InfrastructureRegisteration.cs
+++ E-Commerece.Infrastructure/InfrastructureRegisteration.cs
@@ -1,8 +1,10 @@
+
 using E_Commerece.Core.Interfaces;
 using E_Commerece.Core.Services;
 using E_Commerece.Infrastructure.Data;
 using E_Commerece.Infrastructure.Repositories;
 using E_Commerece.Infrastructure.Repositories.Service;
+using E_Commerece.Infrastructure.Settings;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,9 +21,15 @@ public static class InfrastructureRegisteration
         //unit of work
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         
-        services.AddSingleton<IImageManagementService ,ImageManagementService>();
+        //file provider
         services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
         
+        //file settings
+        services.Configure<FileSettings>(options => configuration.GetSection("FileSettings").Bind(options));
+        
+        //image management service
+        services.AddScoped<IImageManagementService ,ImageManagementService>();
+       
         //db context
         services.AddDbContext<AppDbContext>(option =>
         {

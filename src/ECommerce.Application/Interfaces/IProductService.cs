5898f00f5aa1742101cb13650c8f9d910dffebe6
ShroukOuda <s36568961@gmail.com>
2026-07-05 23:10:39 +0300

    update IProductService.cs

--- src/ECommerce.Application/Interfaces/IProductService.cs
+++ src/ECommerce.Application/Interfaces/IProductService.cs
@@ -2,8 +2,8 @@ namespace ECommerce.Application.Interfaces;
 
 public interface IProductService
 {
-    Task<(IEnumerable<GetProductDTO> Products, int TotalCount)> GetAllProductsAsync(
-        ProductParams productParams, 
+    Task<PaginatedResult<GetProductDTO>> GetAllProductsAsync(
+        ProductSpecParams productSpecParams, 
         CancellationToken ct = default);
     Task<GetProductDTO> GetProductByIdAsync(Guid id, CancellationToken ct = default);
     Task AddProductAsync(AddProductDTO productDto, CancellationToken ct = default);

84a9a0ea3e1dcf2e5db722a12704c2531b0d70d8
ShroukOuda <s36568961@gmail.com>
2026-07-06 14:35:22 +0300

    update ProductVariantMapping.cs

--- src/ECommerce.Application/Mapping/ProductVariantMapping.cs
+++ src/ECommerce.Application/Mapping/ProductVariantMapping.cs
@@ -1,5 +1,6 @@
 using ECommerce.Application.DTO.ProductVariant;
 using ECommerce.Domain.Entities.Products;
+using ECommerce.Domain.Enums.Product;
 
 namespace ECommerce.Application.Mapping;
 
@@ -9,7 +10,7 @@ public class ProductVariantMapping : Profile
     {
         CreateMap<AddProductVariantDTO, ProductVariant>();
         CreateMap<UpdateProductVariantDTO, ProductVariant>()
-            .ForMember(d => d.Status, o => o.MapFrom(s => Enum.Parse<ECommerce.Domain.Enums.Products.ProductVariantStatus>(s.Status)));
+            .ForMember(d => d.Status, o => o.MapFrom(s => Enum.Parse<ProductVariantStatus>(s.Status)));
         CreateMap<ProductVariant, GetProductVariantDTO>()
             .ForMember(d => d.StockStatus, o => o.MapFrom(s => s.StockStatus.ToString()))
             .ForMember(d => d.Status, o => o.MapFrom(s => s.Status.ToString()));

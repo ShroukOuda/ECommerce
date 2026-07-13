624c671b833c0d441a34ea1ea60448b223c551bf
ShroukOuda <s36568961@gmail.com>
2026-07-14 00:02:06 +0300

    update GlobalUsings.cs

--- src/ECommerce.API/GlobalUsings.cs
+++ src/ECommerce.API/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using ECommerce.Application.DTO.Product;
 global using ECommerce.Application.DTO.Category;
 global using Microsoft.AspNetCore.Mvc;
 global using ECommerce.Domain.Specifications;
+global using Microsoft.AspNetCore.Authorization;

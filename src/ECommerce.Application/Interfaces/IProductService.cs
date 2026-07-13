b69a4016e404231e087224a619e0b89f98f2fe2d
ShroukOuda <s36568961@gmail.com>
2026-07-13 15:21:38 +0300

    update IProductService.cs

--- src/ECommerce.Application/Interfaces/IProductService.cs
+++ src/ECommerce.Application/Interfaces/IProductService.cs
@@ -6,6 +6,11 @@ public interface IProductService
         ProductSpecParams productSpecParams, 
         CancellationToken ct = default);
     Task<GetProductDTO> GetProductByIdAsync(Guid id, CancellationToken ct = default);
+    Task<IReadOnlyList<GetProductDTO>> GetFeaturedProductsAsync();
+    Task<IReadOnlyList<GetProductDTO>> GetBestSellerProductsAsync();
+    Task<IReadOnlyList<GetProductDTO>> GetNewArrivalProductsAsync();
+    Task<IReadOnlyList<GetProductDTO>> GetHotDealProductsAsync();
+    Task<IReadOnlyList<GetProductDTO>> GetTopRatedProductsAsync();
     Task AddProductAsync(AddProductDTO productDto, CancellationToken ct = default);
     Task UpdateProductAsync(UpdateProductDTO productDto, CancellationToken ct = default);
     Task DeleteProductAsync(Guid id, CancellationToken ct = default);

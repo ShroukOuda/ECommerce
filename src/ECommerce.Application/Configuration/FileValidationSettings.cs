ca278c61c61a7219dc15f99281d5fe7cd58e33c0
ShroukOuda <s36568961@gmail.com>
2026-07-15 16:26:16 +0300

    update FileValidationSettings.cs

--- src/ECommerce.Application/Configuration/FileValidationSettings.cs
+++ src/ECommerce.Application/Configuration/FileValidationSettings.cs
@@ -4,6 +4,16 @@ public class FileValidationSettings
 {
     public const string SectionName = "FileValidation";
     public int MaxFileSizeInMB { get; set; } = 10;
+    public int MaxIcons { get; set; } = 1;
+    public int MaxBanners { get; set; } = 1;
+    public int MaxThumbnails { get; set; } = 1;
+    public int IconMaxSizeKB { get; set; } = 100;
+    public int BannerMaxSizeMB { get; set; } = 5;
+    public int ThumbnailMaxSizeMB { get; set; } = 2;
+    public List<string> IconAllowedExtensions { get; set; } = new() { ".svg", ".png"};
+    public long IconMaxSizeInBytes => IconMaxSizeKB * 1024;
+    public long BannerMaxSizeInBytes => BannerMaxSizeMB * 1024 * 1024;
+    public long ThumbnailMaxSizeInBytes => ThumbnailMaxSizeMB * 1024 * 1024;
     public List<string> AllowedExtensions { get; set; } = new()
     {
         "jpg", "jpeg", "png"
@@ -12,7 +22,6 @@ public class FileValidationSettings
     public ProductImageValidationSettings ProductImage { get; set; } = new();
     public CategoryImageValidationSettings CategoryImage { get; set; } = new();
     public long MaxFileSizeInBytes => MaxFileSizeInMB * 1024 * 1024;
-  
     
     public bool IsAllowedExtension(string extension) => 
         !string.IsNullOrWhiteSpace(extension) &&

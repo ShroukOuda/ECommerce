08ec6c0d82a9b751f620f35e1c58254c3de257cd
ShroukOuda <s36568961@gmail.com>
2026-07-15 16:26:25 +0300

    update CategoryImageValidationSettings.cs

--- src/ECommerce.Application/Configuration/CategoryImageValidationSettings.cs
+++ src/ECommerce.Application/Configuration/CategoryImageValidationSettings.cs
@@ -2,14 +2,5 @@ namespace ECommerce.Application.Configuration;
 
 public class CategoryImageValidationSettings
 {
-    public int MaxIcons { get; set; } = 1;
-    public int MaxBanners { get; set; } = 1;
-    public int MaxThumbnails { get; set; } = 1;
-    public int IconMaxSizeKB { get; set; } = 100;
-    public int BannerMaxSizeMB { get; set; } = 5;
-    public int ThumbnailMaxSizeMB { get; set; } = 2;
-    public List<string> IconAllowedExtensions { get; set; } = new() { ".svg", ".png"};
-    public long IconMaxSizeInBytes => IconMaxSizeKB * 1024;
-    public long BannerMaxSizeInBytes => BannerMaxSizeMB * 1024 * 1024;
-    public long ThumbnailMaxSizeInBytes => ThumbnailMaxSizeMB * 1024 * 1024;
+    public int MaxTotalPhotos { get; set; } = 3;
 }

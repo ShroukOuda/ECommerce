95c01db98952add365ee60d36249ae26cd5461e0
ShroukOuda <s36568961@gmail.com>
2026-07-15 18:13:41 +0300

    update BrandLogo.cs

--- src/ECommerce.Domain/Entities/Brands/BrandLogo.cs
+++ src/ECommerce.Domain/Entities/Brands/BrandLogo.cs
@@ -1,8 +1,11 @@
+using ECommerce.Domain.Enums.Media;
+
 namespace ECommerce.Domain.Entities.Brands;
 
 public class BrandLogo : BaseImage
 {
     public Guid BrandId { get; set; }
+    public ImageSubType SubType { get; set; }
     
     //Navigation Properties
     public virtual  Brand? Brand { get; set; }
